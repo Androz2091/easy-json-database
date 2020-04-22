1b021f4b075cbea00d467de8592cca6a2fff2a44
Androz2091 <androz2091@gmail.com>
2020-04-22 16:01:23 +0200

    🧪 Update tests

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ const Database = require("./index");
 const db = new Database();
 
 db.set("a", "b");
-console.log(db.get("a"));
+console.log(db.get("a")); // b
 db.delete("a");
-console.log(db.get("a"));
+console.log(db.get("a")); // undefined
