4c24d2f6149681654d1102d2d7751d235e1d36b3
Androz2091 <androz2091@gmail.com>
2021-07-03 18:52:21 +0200

    :sparkles: Add nested properties support
    Closes #4 #3

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const fs = require("fs");
 
+const setNestedProperty = (object, key, value) => {
+    const properties = key.split('.');
+    console.log(properties)
+    let index = 0;
+    for (; index < properties.length - 1; ++index) {
+        object = object[properties[index]];
+    }
+    object[properties[index]] = value;
+}
+
+const getNestedProperty = (object, key) => {
+    const properties = key.split('.');
+    let index = 0;
+    for (; index < properties.length; ++index) {
+        object = object && object[properties[index]];
+    }
+    return object;
+}
+
 module.exports = class EasyJsonDB {
 
     /**
@@ -90,7 +109,7 @@ module.exports = class EasyJsonDB {
      * @param {string} key 
      */
     get(key){
-        return this.data[key];
+        return getNestedProperty(this.data, key);
     }
 
     /**
@@ -98,7 +117,7 @@ module.exports = class EasyJsonDB {
      * @param {string} key 
      */
     has(key){
-        return Boolean(this.data[key]);
+        return Boolean(getNestedProperty(this.data, key));
     }
     
     /**
@@ -107,7 +126,7 @@ module.exports = class EasyJsonDB {
      * @param {*} value 
      */
     set(key, value){
-        this.data[key] = value;
+        setNestedProperty(this.data, key, value);
         this.saveDataToFile();
     }
 
